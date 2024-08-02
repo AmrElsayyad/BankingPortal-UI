a5b3b0e5d6c69399479d5561f16efcfc8aac3222
Amr Elsayyad <amrelsayyad96@gmail.com>
2024-08-02 18:48:03 +0300

    chore: refactor AuthInterceptor to reduce nested conditional logic
    Signed-off-by: Amr Elsayyad <amrelsayyad96@gmail.com>

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,88 +1,109 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpErrorResponse
-} from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
-import { Router } from '@angular/router';
-import { AuthService } from './services/auth.service';
-import { environment } from 'src/environment/environment';
 import { ToastService } from 'angular-toastify';
+import { jwtDecode } from 'jwt-decode';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environment/environment';
 
-import { jwtDecode } from "jwt-decode";
-
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private toastService: ToastService) {}
 
-  constructor(private authService: AuthService, private router: Router, private toastService: ToastService) { }
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
-
-
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     const token = localStorage.getItem(environment.tokenName);
 
     if (token) {
       // Token is present, check if it's valid
-      try {
-        // Decode the JWT token
-        const decodedToken: any = jwtDecode(token);
-
-        // Check if the token is valid and not expired
-        if (decodedToken && decodedToken.exp && decodedToken.exp * 1000 > Date.now()) {
-          // Token is valid and not expired, add it to the headers
-          const authRequest = request.clone({
-            setHeaders: {
-              Authorization: `Bearer ${token}`,
-              'Access-Control-Allow-Origin': environment.origin
-            }
-          });
-          return next.handle(authRequest).pipe(
-            catchError((error: HttpErrorResponse) => {
-              if (error.status === 401) {
-                // Handle 401 Unauthorized error
-                console.error('Unauthorized access. Redirecting to login page...');
-                localStorage.clear();
-                this.router.navigate(['/login']);
-                this.toastService.error('Unauthorized access. Please log in again.');
-              }
-              return throwError(error);
-            })
-          );
-        } else {
-          // Token is expired, clear it and redirect to login page
-          console.error('Session expired. Redirecting to login page...');
-          localStorage.clear();
-          this.router.navigate(['/login']);
-          this.toastService.error('Session expired. Please log in again.');
-          return next.handle(request);
-        }
-      } catch (error) {
-        console.error('Error decoding JWT token:', error);
-        // Token is invalid, clear it and redirect to login page
-        console.error('Invalid token. Redirecting to login page...');
-        localStorage.clear();
-        this.router.navigate(['/login']);
-        this.toastService.error('Invalid token. Please log in again.');
-        return next.handle(request);
+      if (this.isTokenValid(token)) {
+        return this.handleValidToken(request, next, token);
+      } else {
+        return this.handleExpiredToken(request, next);
       }
     } else {
       // No token present, allow the request as it is
-      return next.handle(request).pipe(
-        catchError((error: HttpErrorResponse) => {
-          if (error.status === 401) {
-            // Handle 401 Unauthorized error
-            console.error('Unauthorized access. Redirecting to login page...');
-            this.router.navigate(['/login']);
-            this.toastService.error('Unauthorized access. Please log in again.');
-          }
-          return throwError(error);
-        })
-      );
+      return this.handleNoToken(request, next);
     }
+  }
+
+  private isTokenValid(token: string): boolean {
+    try {
+      const decodedToken: any = jwtDecode(token);
+
+      return (
+        decodedToken && decodedToken.exp && decodedToken.exp * 1000 > Date.now()
+      );
+    } catch (error) {
+      console.error('Error decoding JWT token:', error);
+
+      return false;
+    }
+  }
+
+  private handleValidToken(
+    request: HttpRequest<unknown>,
+    next: HttpHandler,
+    token: string
+  ): Observable<HttpEvent<unknown>> {
+    const authRequest = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+        'Access-Control-Allow-Origin': environment.origin,
+      },
+    });
+
+    return next.handle(authRequest).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Handle 401 Unauthorized error
+          console.error('Unauthorized access. Redirecting to login page...');
+          localStorage.clear();
+          this.router.navigate(['/login']);
+          this.toastService.error('Unauthorized access. Please log in again.');
+        }
+
+        return throwError(() => error);
+      })
+    );
+  }
+
+  private handleExpiredToken(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    console.error('Session expired. Redirecting to login page...');
+    localStorage.clear();
+    this.router.navigate(['/login']);
+    this.toastService.error('Session expired. Please log in again.');
+
+    return next.handle(request);
+  }
+
+  private handleNoToken(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Handle 401 Unauthorized error
+          console.error('Unauthorized access. Redirecting to login page...');
+          this.router.navigate(['/login']);
+          this.toastService.error('Unauthorized access. Please log in again.');
+        }
+
+        return throwError(() => error);
+      })
+    );
   }
 }
